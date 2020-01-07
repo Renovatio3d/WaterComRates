d0b193038d6c054455638e3b3baf6016d5527baf
Shallu Nah <renovatio3d@gmail.com>
2020-01-06 22:12:46 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// AUTHOR: Shallu Nah                                                                                                                                                                                  //
+// PROGRAM DESCRIPTION: The program calculate and print out bills for the city water company.
+// INPUT: Reads the The customer 4-digit account number, the type of water services, home, commercial,
+// or industrial, and the amount of gallons require.
+// PROCESSING: The program calculates the costs base on the type of account and amount of gallons of water used.
+// OUTPUT: The program output the chosen account plan, the number of gallons of water and the total bill.
+
+#include <iostream>            // for I/O, such ASCII
+#include <iomanip>            //  for formatting output
+#include <cmath>             //   for using pow , such math functions
+#include <cctype>           //    Character handling functions, if any.
+
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
